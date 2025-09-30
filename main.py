55bcfcd47509bcfa30e1ff41ea02bb66d95200cf
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2025-09-30 16:16:41 +0300

    add Reports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, get_chat_list
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,7 +9,17 @@ def main():
     text = get_book_text(path)
     num_words = get_word_count(text)
     char_count_dict = get_char_count(text)
+    sorted_list = get_chat_list(char_count_dict)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(char_count_dict)
+    print("--------- Character Count -------")
+    for i in sorted_list:
+        if i["char"].isalpha():
+            print(f"{i['char']}: {i['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,9 +2,23 @@ def get_word_count(text):
     return len(text.split())
 
 def get_char_count(text):
-    char_dict = {chr(x):0 for x in range(ord('a'), ord('z')+1)}
+    char_dict = {}
 
     for char in text.lower():
-        if char in char_dict:
+        if char not in char_dict:
+            char_dict.update({char: 1})
+        else:
             char_dict[char] += 1
     return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def get_chat_list(char_dict):
+    char_list = []
+    for key in char_dict:
+        char_list.append({"char": key, "num": char_dict[key]})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+

99221384b52c8e6a7cc9a08edb586ae1308423f7
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2025-10-05 16:29:21 +0300

    a description of your changes here

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count, get_char_count, get_chat_list
 
 def get_book_text(path):
@@ -5,7 +6,10 @@ def get_book_text(path):
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_word_count(text)
     char_count_dict = get_char_count(text)
